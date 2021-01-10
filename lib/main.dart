be451f70273ee6878470b96fa11382333f4458f8
mashazavolnyuk <mashazavolnyuk@yandex.ru>
2021-01-10 14:06:30 +0200

    Colours & Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,26 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: Text("Start application"),
+        title: Text(
+          "Start application",
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.deepPurple[600],
+            fontFamily: "PottaOne"
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.amberAccent,
       ),
       body: Center(
         child: Text("Center text"),
       ),
       floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.amberAccent,
         child: Text("click"),
       ),
     ),
   ));
 }
-
