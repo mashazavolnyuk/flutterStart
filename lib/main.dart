789c88704ef1eae3c0711aaabf39daa6f2b24813
mashazavolnyuk <mashazavolnyuk@yandex.ru>
2021-01-11 20:07:29 +0200

    Moved code to StateleesWidget Added assets

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,36 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: Home()
+  ));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text(
           "Start application",
           style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.deepPurple[600],
-            fontFamily: "PottaOne"
-          ),
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.deepPurple[600],
+              fontFamily: "PottaOne"),
         ),
         centerTitle: true,
         backgroundColor: Colors.amberAccent,
       ),
       body: Center(
-        child: Text("Center text"),
+        child: Image(
+          image: AssetImage("assets/flutter.jpg"),
+        )
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         backgroundColor: Colors.amberAccent,
         child: Text("click"),
       ),
-    ),
-  ));
+    );
+  }
 }
